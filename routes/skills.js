f8bee03e1a581f673fc33c6594b405550d309dba
avilla13 <xetox13@gmail.com>
2023-06-28 22:57:46 -0500

    Update views, controllers, models and routes

--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,15 @@
+// routes/skills.js
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/skills', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// ALl actual paths start with '/skills'
+// Require the controller that exports Skills CRUD functions
+const skillsCtrl = require('../controllers/skills');
 
 module.exports = router;
+
+// GET /skills
+router.get('/', skillsCtrl.index);
+
+//GET /skills/:id
+router.get('/:id', skillsCtrl.show);
